a5d5d470ca791c72d3b23481db3d646aaa48889a
aatmajaraval21 <82444962+aatmajaraval21@users.noreply.github.com>
2021-07-26 15:02:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+menu_list_array = ["Veg Margherita Pizza","cheese burst pizza","mexican cheese pizza","mushroom with panner pizza","paneer chesse brust pizza"
+                    ];
+var i=0;
+function getmenu()
+{
+    i++;
+  var names_pizza=5;
+  if (i>names_pizza)
+  {
+      i=0;
+      
+  }
+}
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+
+
+
+
+function add_top(){
+
+}
